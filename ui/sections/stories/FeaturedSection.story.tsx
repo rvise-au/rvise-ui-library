5d0606082ed6b6b4441c0f48cb7fb001931bb6ac
Igor Revenko <irevdev@gmail.com>
2024-01-14 21:57:35 +1100

    initial commit

--- ui/sections/stories/FeaturedSection.story.tsx
+++ ui/sections/stories/FeaturedSection.story.tsx
@@ -52,3 +52,48 @@ export const SplitFeaturedOffsetImage = () => (
     />
   </Section>
 );
+
+export const StackedCentered = () => (
+  <Section>
+    <SplitWithImage
+      isOffset
+      textBlock={{
+        ...textBlockMock,
+        children: (
+          <>
+            <FeaturedBoxGroup
+              cols={{
+                base: 1,
+                xs: 1,
+              }}
+              margins={{
+                lg: '35px 0',
+                md: '24px 0',
+                sm: '20px 0',
+              }}
+              verticalSpacing={32}
+              style={{
+                maxWidth: 980,
+              }}
+            >
+              {featuresMock.map((item, index) => (
+                <FeatureBox
+                  key={index}
+                  iconSize={28}
+                  icon={item.icon}
+                  heading={item.heading}
+                  text={item.text}
+                  iconBackgroundColor="green"
+                  iconVariant="filled"
+                  iconRadius="sm"
+                />
+              ))}
+            </FeaturedBoxGroup>
+            {textBlockMock.children}
+          </>
+        ),
+      }}
+      mediaBlock={mediaOffsetBlockMock}
+    />
+  </Section>
+);
